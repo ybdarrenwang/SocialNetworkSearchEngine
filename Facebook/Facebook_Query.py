275b4d33044810cdddfee0916a7c33719b105050
piscesfantasy <piscesfantasy@gmail.com>
2016-01-03 16:48:33 -0500

    Wrote preliminary Facebook API scripts in Python

--- Facebook/Facebook_Query.py
+++ Facebook/Facebook_Query.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python
 # coding: utf-8
-import tweepy
+import facebook
 import sys, os, json
 from optparse import OptionParser
 
 reload(sys)  
 sys.setdefaultencoding('utf8')
-	
-consumer_key="2YIdiuX1g8GyCGu0bkl2w"
-consumer_secret="NQm7aC0R7yYNyIgIGp9YkVl5whgqLr7yhNM7n13sCso"
-tmp_folder = "log/twitter"
+
+app_id       = '1226220920740227'
+app_secret   = '24f99662865dd4302974a4d1ee8f5ce2'
+perms        = ['user_friends','manage_pages','publish_pages','user_posts']
+redirect_uri = "https://www.facebook.com/"
+tmp_folder = "log/facebook"
 
 def userLogin(user_name):
     all_users_auth = {}
@@ -24,17 +26,14 @@ def userLogin(user_name):
     else:
         exit("ERROR: Please login with --login [USERNAME].")
 
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     if not current_auth: # new user, ask for auth and save to cache
-        print auth.get_authorization_url()
-        verification_num = str(input("Please input the verification number: "))
-        auth.get_access_token( verification_num )
-        current_auth["ACCESS_TOKEN"] = auth.access_token
-        current_auth["ACCESS_TOKEN_SECRET"] = auth.access_token_secret
+        print facebook.auth_url(app_id, redirect_uri, perms)
+        code = str(raw_input("Please copy and paste the verification number following \"https://www.facebook.com/?code=\": "))
+        access_token = facebook.get_access_token_from_code(code, redirect_uri, app_id, app_secret)["access_token"]
+        current_auth["ACCESS_TOKEN"] = access_token
         all_users_auth[user_name] = current_auth
         json.dump(all_users_auth, open(tmp_folder+'/users.json', 'w'))
-    auth.set_access_token(current_auth["ACCESS_TOKEN"], current_auth["ACCESS_TOKEN_SECRET"])
-    return tweepy.API(auth)
+    return facebook.GraphAPI(current_auth["ACCESS_TOKEN"])
 
 def currentUserLogin():
     try:
@@ -43,60 +42,40 @@ def currentUserLogin():
     except:
         exit("ERROR: Can't find user login info. Please login with --login [USERNAME].")
 
-def getTime(date):
-    Time = {}
-    Time['year'] = date.year
-    Time['month'] = date.month
-    Time['day'] = date.day
-    Time['hour'] = date.hour
-    Time['minute'] = date.minute
-    Time['second']= date.second
-    Time['weekday'] = date.weekday()
-    return Time
-
-def getPosts(public_tweets):
-    posts = []
-    for tweet in public_tweets:
-        post = {}
-        post['user_name'] = tweet.author.screen_name
-        post['id'] = tweet.id_str
-        post['content'] = tweet.text
-        post['time'] = getTime(tweet.created_at)
-        posts.append(post)
-    return posts
+def processPosts(my_posts):
+    for post in my_posts:
+        if 'message' not in post:
+            post['message'] = ''
+        if 'story' not in post:
+            post['story'] = ''
+    return my_posts
 
 def getMyTimeline(api):
-    return getPosts(api.home_timeline(count=100))
+    return processPosts(api.get_object('me/feed')['data'])
 
 def getUserTimeline(api, name):
-    return getPosts(api.user_timeline(screen_name=name,count=100))
-
-def queryAll(api, query):
-    return getPosts(api.search(query,{}))
+    return [p for p in getMyTimeline(api) if unicode(name, "utf-8") in p['message'] or unicode(name, "utf-8") in p['story']]
 
 def queryMyTimeline(api, query):
-    return [p for p in getMyTimeline(api) if unicode(query, "utf-8") in p['content']]
+    return [p for p in getMyTimeline(api) if unicode(query, "utf-8") in p['message'] or unicode(query, "utf-8") in p['story']]
 
 def queryUserTimeline(api, user_name, query):
-    return [p for p in getUserTimeline(api, user_name) if unicode(query, "utf-8") in p['content']]
+    return [p for p in getUserTimeline(api, user_name) if unicode(query, "utf-8") in p['message'] or unicode(query, "utf-8") in p['story']]
 
-def favorite(api, tweet_id):
-    return api.create_favorite(tweet_id)
+def favorite(api, post_id):
+    api.put_like(post_id)
 
-def reply(api, tweet_id, text):
-    user = api.get_status(tweet_id)
-    content = '@' + user.author.screen_name + ': '+ unicode(text,'utf8')  
-    public_tweets = api.update_status(status=content, in_reply_to_status_id=tweet_id)
-
-def saveFriendsList(api):
-    open('log/friends.list','w').write('\n'.join([f.screen_name for f in api.friends()]))
+def reply(api, post_id, text):
+    api.put_comment(post_id, text)
 
 def savePosts(posts):
     open(tmp_folder+'/posts.json', 'w').write(json.dumps(posts))
 
+
 if __name__ == '__main__':
     """
     Use cached user as default; override cached user by --login USER_ID
+    Note: Facebook API only allows to read/write current user's wall
     """
     os.system('mkdir -p '+tmp_folder)
     parser = OptionParser()
@@ -106,8 +85,6 @@ if __name__ == '__main__':
                         help="Search with --query, --user or --own")
     parser.add_option("--query", dest="query", default=None, help="The query to search")
     parser.add_option("--user", dest="user", default=None, help="The user ID to search")
-    parser.add_option("--own", dest="onlySearchMyposts", action="store_true", default=False,
-                        help="Limit search to current user's posts")
     parser.add_option("--reply", dest="reply", default=None,
                         help="Reply the post")
     parser.add_option("--like", dest="like", default=None,
@@ -115,7 +92,6 @@ if __name__ == '__main__':
     (options, args) = parser.parse_args()
     if options.login: # override cached user
         user = userLogin(options.login)
-        saveFriendsList(user)
     elif options.search:
         posts = None
         if options.user:
@@ -124,11 +100,8 @@ if __name__ == '__main__':
             else:
                 posts = getUserTimeline(currentUserLogin(), options.user)
         elif options.query:
-            if options.onlySearchMyposts:
-                posts = queryMyTimeline(currentUserLogin(), options.query)
-            else:
-                posts = queryAll(currentUserLogin(), options.query)
-        elif options.onlySearchMyposts: 
+            posts = queryMyTimeline(currentUserLogin(), options.query)
+        else: 
             posts = getMyTimeline(currentUserLogin())
         savePosts(posts)
     elif options.reply:
--- README.md
+++ README.md
@@ -4,8 +4,11 @@ SocialNetworkSearchEngine
 This was my final project for grad-school class "Design Pattern and Software Development" of National Taiwan University (NTU) in Spring 2012. This project was done in pair-programming. My partner, Yung-Hsiang Yang (Github ID: larry61410) implemented API calls to social networks with Python. And I, Yow-Bang Wang (Github ID: piscesfantasy), was in charge of framework design the remaining implementations with C++. The first version support search on Plurk and Twitter. Main functions include keyword/friend search, browsing search results, recommendation and reply.
 
 [12.2015]
-- Rewrote Twitter part (both C++ and Python) for new Twitter API and refactoring
-- Removed Plurk
+- Rewrote Twitter part (both C++ and Python) for new Twitter API and refactoring.
+- Removed Plurk.
+
+[01.2016]
+- Wrote preliminary Facebook API scripts in Python. To hook with C++ main program.
 
 [Known issues]
 - Enter without content in most situations would keep program waiting without hints output, makes it look like freezing.
